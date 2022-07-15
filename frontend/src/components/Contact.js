470387b68799f2b1e179711172bb7a139e7f20ce
Yash Joshi <yashjoshimail@gmail.com>
2022-07-15 20:25:45 +0530

    Setting up the router and Project files

---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+    <h1>
+    This page is about us.
+    </h1>
+    </>
+  )
+}
+
+export default Contact
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>
+          <h1>
+            This is the home of the website.
+            </h1>  
+            </h1>
+    </div>
+  )
+}
+
+export default Home
