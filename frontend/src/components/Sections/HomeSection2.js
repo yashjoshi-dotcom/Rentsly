07f7ffa993aff123a80ad3d0778b660bc6fa5b5e
Simarjeet Singh <sjthecoder@gmail.com>
2022-07-31 01:50:43 +0530

    Section 2 first design

--- frontend/src/components/Sections/HomeSection2.js
+++ frontend/src/components/Sections/HomeSection2.js
@@ -1,13 +1,15 @@
 import HomeCard from "../UI/Section2/HomeCard";
 import styles from "./HomeSection2.module.css";
+import FeaturesDetails from "../UI/Section2/FeaturesDetails";
 
 function Section2(params) {
 	return (
 		<div className={`${styles.grid_display} + ${styles.section2}`}>
 			<HomeCard designClass={styles.home_card1} />
-            <HomeCard designClass={styles.home_card2} />
-            <HomeCard designClass={styles.home_card3} />
-            <HomeCard designClass={styles.home_card4} />
+			<HomeCard designClass={styles.home_card2} />
+			<HomeCard designClass={styles.home_card3} />
+			<HomeCard designClass={styles.home_card4} />
+			<FeaturesDetails />
 		</div>
 	);
 }
