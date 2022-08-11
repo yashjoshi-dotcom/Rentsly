1b420f23c50b5e6f9100e711286ad57cf66600c9
Yash Joshi <yashjoshimail@gmail.com>
2022-08-11 18:28:24 +0530

    added sample assets and search page

---
+++ frontend/src/components/HeaderSearch/HeaderSearch.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import image from "../HouseData/assets/img/house-banner.png"
+
+const HeaderSearch = () => {
+  return (
+    <div>
+        <div>
+        This is where the Header needs to come 
+        </div>
+        <div>
+        <img src= {image} alt="No image" />
+        </div>
+    </div>
+  )
+}
+
+export default HeaderSearch 
--- frontend/src/pages/search.jsx
+++ frontend/src/pages/search.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import HouseSnip from "../components/Cards/HouseSnip";
+import HeaderSearch from "../components/HeaderSearch/HeaderSearch";
+import SearchBar from "../components/SearchBar/SearchBar";
 
 const search = () => {
 	return (
 		<>
 			<h1>This is the Search Page</h1>
+			<HeaderSearch/>
+			<SearchBar/>
+			<HouseSnip/>
+
 		</>
 	);
 };
