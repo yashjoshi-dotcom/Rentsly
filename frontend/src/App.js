e928153a7c2bf1aae774d3b3e6b1d9795810f070
Simarjeet Singh <sjthecoder@gmail.com>
2022-08-12 19:33:44 +0530

    fix max-width

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,29 @@
-import React from 'react' 
-import {Route, Routes} from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-//Importing all items 
-import Navbar from './components/UI/navbar/Navbar'
+//Importing all items
+import Navbar from "./components/UI/navbar/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Register from "./pages/Register";
 import Signin from "./components/SignIn/Signin";
-import Contact from "./pages/Contact"
-import Search from "./pages/search"
+import Contact from "./pages/Contact";
+import Search from "./pages/search";
 
 const App = () => {
-  return (
-    <div className='max-w-[1440px] mx-auto bg-white'>  
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/search" element={<Search />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/signin" element={<Signin />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+		</div>
+	);
+};
 
-        <Navbar/>
-        <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/contact" element={<Contact />} />
-        </Routes>
-    </div>
-  )
-}
-
-export default App
+export default App;
