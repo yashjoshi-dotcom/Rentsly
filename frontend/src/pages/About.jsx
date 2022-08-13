1929d78f8514c0f54d2b9defad1a974a05f7478c
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2022-08-13 23:17:31 +0530

    AboutUs 2.0

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,26 +1,69 @@
 import React from "react";
-import styled from "styled-components";
-import style from "./About.css";
-import illus from "../data/AboutUsImages/illus.svg"
+import illus from "../data/AboutUsImages/illus.svg";
 
 const About = () => {
   return (
-    <div className="Global">
-      <div className="Content">
-        <img src={illus} alt="about-us-image" />
-        <div className="Text">
-          <h1>
-            -We <span className="Heart"> ❤️ </span> what We Do-
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea
-          </p>
-          <button>Home</button>
+    <div>
+      <div className="w-full h-[40rem]">
+        <div className="h-[10rem] pt-16 text-center w-[50rem] mx-auto text-3xl">
+          <h1>We Design Build and ship world class digital products</h1>
+        </div>
+        <div>
+          <div className="w-[35%] float-left h-[30rem] text-center">
+            <ul className="pt-[3rem]">
+              <ul>
+                <li>
+                  <h1 className="text-[4rem] font-medium">150+</h1>
+                </li>
+                <li>
+                  <h3 className="text-[1.5rem] text-gray-500 mb-[3rem]  ">
+                    Website & Apps launched
+                  </h3>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <h1 className="text-[4rem] font-medium">50+</h1>
+                </li>
+                <li>
+                  <h3 className="text-[1.5rem] text-gray-500 mb-[3rem]">
+                    Awards & recognition
+                  </h3>
+                </li>
+              </ul>
+            </ul>
+          </div>
+          <div className="w-[30%] float-left h-[30rem] text-center">
+            <img src={illus} alt="description-image" />
+          </div>
+          <div className="w-[35%] float-right h-[30rem] text-center">
+            <ul className="pt-[3rem]">
+              <ul>
+                <li>
+                  <h1 className="text-[4rem] font-medium">100%</h1>
+                </li>
+                <li>
+                  <h3 className="text-[1.5rem] text-gray-500 mb-[3rem]">
+                    in-house & independent
+                  </h3>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <h1 className="text-[4rem] font-medium">Ind</h1>
+                </li>
+                <li>
+                  <h3 className="text-[1.5rem] text-gray-500 mb-[3rem]">
+                    working globally, proudly indian
+                  </h3>
+                </li>
+              </ul>
+            </ul>
+          </div>
         </div>
       </div>
+      <div className="w-full h-[40rem]">section-2</div>
+      <div className="w-full h-[40rem]">section 3</div>
     </div>
   );
 };
