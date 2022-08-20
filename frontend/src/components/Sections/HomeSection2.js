1bbc7a82765c5b00200e81cae3d5e436b009a156
yashjoshi-dotcom <77540753+yashjoshi-dotcom@users.noreply.github.com>
2022-08-20 19:55:55 +0530

    fixed coloring scheme

--- frontend/src/components/Sections/HomeSection2.js
+++ frontend/src/components/Sections/HomeSection2.js
@@ -3,7 +3,7 @@ import HomeCard from "../UI/Section2/HomeCard";
 function Section2(props) {
 	return (
 		<div
-			className={`flex flex-col justify-evenly p-12 relative h-screen w-full bg-section2bgcolor1 `}
+		className={`flex flex-col justify-evenly p-12 relative h-screen w-full bg-section2bgcolor1 `}
 		>
 			<h1 className="text-5xl text-center text-textcolor1">Random heading</h1>
 			<p className="text-center italic text-textcolor1">Lodwe ipsum wala paragraph </p>
